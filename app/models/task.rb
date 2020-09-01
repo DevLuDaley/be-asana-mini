003bbbf0faeef814e330bf51275dc8e64997956d
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2020-09-01 12:14:45 -0400

    + belongs_to: project

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,8 @@
 class Task < ApplicationRecord
 belongs_to :user
-  has_many :subtasks
-  accepts_nested_attributes_for :subtasks, allow_destroy: true
-  has_one :note
-  accepts_nested_attributes_for :note, allow_destroy: true
+belongs_to :project
+has_many :subtasks
+accepts_nested_attributes_for :subtasks, allow_destroy: true
+has_one :note
+accepts_nested_attributes_for :note, allow_destroy: true
 end
